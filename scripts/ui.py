155f11caae6c39b4108ed9ea5f8cbeeaf504925d
laksjdjf <aikasama@hotmail.co.jp>
2023-02-09 18:18:15 +0900

    Update ui.py

--- scripts/ui.py
+++ scripts/ui.py
@@ -13,6 +13,7 @@ from modules.processing import StableDiffusionProcessing
 
 import ldm.models.diffusion.ddim as ddim
 from scripts.ddim import ABaGDDIMSampler
+from scripts.attention_controll import AttentionController
 
 
 class OrgDDIMSampler(ddim.DDIMSampler):
@@ -33,11 +34,26 @@ class Script(scripts.Script):
                 """)
         with gr.Row():
             enable_abag = gr.Checkbox(label='Enable ABaG', value=False)
-        return [ebable_abag]
+            attention_division = gr.Dropdown([2,4,8], label=f"set size of attention map to (h/n,w/n)", value=4)
+        with gr.Row():
+            bboxes = gr.Textbox(lines = 5, label="bboxes")
+
+        with gr.Row():    
+            lr = gr.Slider(0, 2.0, value=0.6,step=0.1,label='learning(?) rate (multiplier of grad)')
+        return [enable_abag, attention_division, bboxes, lr]
     
     
-    def run(self, p: StableDiffusionProcessing, enable_abag):
+    def run(self, p: StableDiffusionProcessing, enable_abag, attention_division, bboxes, lr):
         if enable_abag:
+            attention_controller = AttentionController(
+                unet = p.sd_model.model.diffusion_model, 
+                steps = p.steps,
+                bboxes = bboxes,
+                attention_height = p.height // attention_division // 8,
+                attention_width = p.width // attention_division // 8,
+                lr = lr,
+            )
+            setattr(ABaGDDIMSampler,"attention_controller",attention_controller)
             #DDIMSamplerを改造する
             ddim.DDIMSampler = ABaGDDIMSampler
             #モジュールのリロードで無理やり上の変更を反映
