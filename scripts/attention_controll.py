61de94b33bd4fa34c856c20b22e401bd9ccff669
laksjdjf <aikasama@hotmail.co.jp>
2023-02-10 17:46:52 +0900

    Update attention_controll.py

--- scripts/attention_controll.py
+++ scripts/attention_controll.py
@@ -5,6 +5,8 @@ import itertools
 from typing import List, Optional, Union, Tuple
 import functools
 
+import copy
+
 from scripts.gaussian_smoothing import GaussianSmoothing
 
 import torch
@@ -15,23 +17,23 @@ from einops import rearrange, repeat
 import numpy as np
 
 class AttentionController:
-    def __init__(self, unet:UNetModel, steps:int, bboxes:list, attention_height:int = 16, attention_width:int = 16,lr:float = 0.6):
+    def __init__(self, unet, steps:int, bboxes:list, attention_height:int = 16, attention_width:int = 16,lr:float = 0.6):
         '''
         unet:unet
-        attention_division: Set size of attention map to (h/8/n, w/8/n) 
         '''
         self.unet = unet
         self.steps = steps
         self.scale_range = np.linspace(1., 0.5, self.steps)
-        
+        self._hook_blocks()
         self.attention_maps = None
         self.attention_height = attention_height
         self.attention_width = attention_width
         self.attention_pixels = attention_height * attention_width
-        self._hook_blocks()
+
         self.lr = lr #学習してないだろ
         self.bboxes = [[int(s) for s in box.split(" ")] for box in bboxes.split("\n")]
         self.indices_to_alter = [box[0] for box in  self.bboxes]
+        self.max_indices = max(self.indices_to_alter)
         print(self.bboxes)
         ##よく分からないものたち gaussian_smoothing関連だと思うけど
         self.sigma = 0.5
@@ -50,19 +52,31 @@ class AttentionController:
                 if type(module) is SpatialTransformer:
                     spatial_transformer = module
                     for basic_transformer_block in spatial_transformer.transformer_blocks:
-                        basic_transformer_block.attn2.forward = functools.partial(_hook_forward, basic_transformer_block.attn2, self) #forward書き換え
-
+                        new_forward = self._hook_forward_create()
+                        basic_transformer_block.attn2.forward = new_forward.__get__(basic_transformer_block.attn2, CrossAttention)
     
     def _reset(self):
         self.attention_maps = None
-    
+
+    def _get_attention_map(self, sim:torch.Tensor):
+        if sim.shape[1] != self.attention_pixels:
+            return
+        attention_map = sim.reshape(1,-1,self.attention_height,self.attention_width,sim.shape[-1])
+        if self.attention_maps is None:
+            self.attention_maps = attention_map
+        else:
+            self.attention_maps = torch.cat([self.attention_maps,attention_map])
+
     def _get_avarage_attention(self):
-        return self.attention_maps.sum(0) / self.attention_maps.shape[0]
+        return (self.attention_maps.sum(0) / self.attention_maps.shape[0]).mean(0)
 
     def _compute_max_attention_per_bbox_and_outofbbox(self,attention_map_mean) -> List[torch.Tensor]:
+        print(attention_map_mean.shape)
 
-        attention_for_text = attention_map_mean[:, :, 1:-1]
-        attention_for_text *= 100
+        #webuiは勝手に77までパディングするので注意するトークンまでに限定
+        attention_for_text = attention_map_mean[:, :, 1:self.max_indices]
+        print(attention_for_text.shape)
+        attention_for_text = attention_for_text * 100
         attention_for_text = torch.nn.functional.softmax(attention_for_text, dim=-1)
 
         # Shift indices since we removed the first token
@@ -110,55 +124,50 @@ class AttentionController:
             return loss, in_bbox_losses, out_bbox_losses
         else:
             return loss
-    
+
     def _update_latent(self,latents: torch.Tensor, loss: torch.Tensor, step_size: float) -> torch.Tensor:
         grad_cond = torch.autograd.grad(loss.requires_grad_(True), latents, retain_graph=True)[0]
+        print(loss, grad_cond.mean(),step_size)
         latents = latents - step_size * grad_cond
         return latents
 
     
     
-#attention mapを保存するようhookする
-def _hook_forward(_self, attention_controller, x, context=None, mask=None):
-    h = _self.heads
+    #attention mapを保存するようhookする
+    def _hook_forward_create(self):
+        def _hook_forward(_self, x:torch.Tensor, context:torch.Tensor=None, mask=None):
+            h = _self.heads
+            q = _self.to_q(x)
+            context = default(context, x)
+            k = _self.to_k(context)
+            v = _self.to_v(context)
 
-    q = _self.to_q(x)
-    context = default(context, x)
-    k = _self.to_k(context)
-    v = _self.to_v(context)
+            q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> (b h) n d', h=h), (q, k, v))
 
-    q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> (b h) n d', h=h), (q, k, v))
+            # force cast to fp32 to avoid overflowing
+            # 応急処置ｗ
+            if False:
+                with torch.autocast(enabled=False, device_type = 'cuda'):
+                    q, k = q.float(), k.float()
+                    sim = einsum('b i d, b j d -> b i j', q, k) * _self.scale
+            else:
+                sim = einsum('b i d, b j d -> b i j', q, k) * _self.scale
 
-    # force cast to fp32 to avoid overflowing
-    # 応急処置ｗ
-    if False:
-        with torch.autocast(enabled=False, device_type = 'cuda'):
-            q, k = q.float(), k.float()
-            sim = einsum('b i d, b j d -> b i j', q, k) * _self.scale
-    else:
-        sim = einsum('b i d, b j d -> b i j', q, k) * _self.scale
-    
-    del q, k
+            if exists(mask):
+                mask = rearrange(mask, 'b ... -> b (...)')
+                max_neg_value = -torch.finfo(sim.dtype).max
+                mask = repeat(mask, 'b j -> (b h) () j', h=h)
+                sim.masked_fill_(~mask, max_neg_value)
 
-    if exists(mask):
-        mask = rearrange(mask, 'b ... -> b (...)')
-        max_neg_value = -torch.finfo(sim.dtype).max
-        mask = repeat(mask, 'b j -> (b h) () j', h=h)
-        sim.masked_fill_(~mask, max_neg_value)
+            # attention, what we cannot get enough of
+            sim = sim.softmax(dim=-1)
+            
+            ######変換部分##########################################
+            # save attention map
+            self._get_attention_map(sim)
+            ########################################################
 
-    # attention, what we cannot get enough of
-    sim = sim.softmax(dim=-1)
-    
-    ######変換部分##########################################
-    # save attention map
-    if sim.shape[1] == attention_controller.attention_pixels:
-        attention_map = sim.reshape(1,-1,attention_controller.attention_height,attention_controller.attention_width,sim.shape[-1])   
-        if attention_controller.attention_maps is None:
-            attention_controller.attention_maps = attention_map
-        else:
-            attention_controller.attention_maps = torch.cat([attention_controller.attention_maps,attention_map])
-    ########################################################
-
-    out = einsum('b i j, b j d -> b i d', sim, v)
-    out = rearrange(out, '(b h) n d -> b n (h d)', h=h)
-    return _self.to_out(out)
+            out = einsum('b i j, b j d -> b i d', sim, v)
+            out = rearrange(out, '(b h) n d -> b n (h d)', h=h)
+            return _self.to_out(out)
+        return _hook_forward
